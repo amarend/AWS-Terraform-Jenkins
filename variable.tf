b907002e5bd11cad92270b6435caa523604a24f7
Akanksha Amarendra <88309876+amarend@users.noreply.github.com>
2025-04-14 18:24:40 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID to use for the EC2 instance"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0" # Replace with your preferred AMI
+}
