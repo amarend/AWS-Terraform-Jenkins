f8df1d472103fad01584910085085dba11a666ea
Akanksha Amarendra <88309876+amarend@users.noreply.github.com>
2025-04-14 18:25:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,10 @@
 provider "aws" {
-  region     = "us-east-1"
+  region     = var.region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Update with your preferred AMI
-  instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
 }
-
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
